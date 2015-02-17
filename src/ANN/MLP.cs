b99acd221a449496a1e2b6cf7fb599922da44c1b
Jake Cowton <rockjake28@hotmail.co.uk>
2015-02-17 15:48:03 +0000

    ANN: Created ANN Project
    - The MLP class will be used to classify the gestures

---
+++ src/ANN/MLP.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyKinectInterface.ANN
+{
+    public class MLP
+    {
+        public MLP()
+        {
+            Console.WriteLine("MLP Accessed");
+        }
+    }
+}
