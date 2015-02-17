030135f61e8d3771f1d39d014fca4573d16c2ea6
Jake Cowton <rockjake28@hotmail.co.uk>
2015-02-17 14:33:14 +0000

    Setup: Set up the configuration for the app
    - The window and `.cs` file that controls it are now cooperating

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -4,7 +4,7 @@
 // </copyright>
 //------------------------------------------------------------------------------
 
-namespace Microsoft.Samples.Kinect.SkeletonBasics
+namespace SpotifyKinectInterface
 {
     using System.Windows;
 
--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Kinect for Windows Developer Toolkit SkeletonBasics - WPF")]
+[assembly: AssemblyTitle("Spotify Kinect Interface")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("")]
