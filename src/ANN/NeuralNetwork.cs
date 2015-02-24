b6af54b6cb9d81d5c920a22300bf3db8a865575f
Ben Fielding <benfielding@live.co.uk>
2015-02-24 21:12:35 +0000

    Commented MLP code to add docstrings.

--- src/ANN/NeuralNetwork.cs
+++ src/ANN/NeuralNetwork.cs
@@ -6,13 +6,47 @@ using System.Threading.Tasks;
 
 namespace ANN
 {
+    /// <summary>
+    /// Bring the layers together
+    /// </summary>
     class NeuralNetwork
     {
 
+        /// <summary>
+        /// The input layer of the MLP
+        /// </summary>
         private NeuronLayer inputLayer;
+
+        /// <summary>
+        /// The hidden layer of the MLP
+        /// </summary>
         private NeuronLayer hiddenLayer;
+
+        // TODO: Modify this class to allow multiple hidden layers (SHOULD be minor changes to this class only)
+
+        /// <summary>
+        /// The output layer of the MLP
+        /// </summary>
         private NeuronLayer outputLayer;
 
+        /// <summary>
+        /// Constructor for the neural network class
+        /// </summary>
+        /// <param name="numberInputs">
+        /// The number of nodes in the input layer
+        /// </param>
+        /// <param name="numberHiddenNodes">
+        /// The number of nodes in the hidden layer
+        /// </param>
+        /// <param name="numberOutputs">
+        /// The number of nodes in the output layer
+        /// </param>
+        /// <param name="learningRate">
+        /// The learning rate of the network
+        /// </param>
+        /// <param name="momentumFactor">
+        /// The momentum factor of the network
+        /// </param>
         public NeuralNetwork(int numberInputs, int numberHiddenNodes, int numberOutputs, float learningRate, float momentumFactor)
         {
             inputLayer = new NeuronLayer(numberInputs, numberHiddenNodes, 0, learningRate, momentumFactor);
@@ -24,16 +58,37 @@ namespace ANN
             outputLayer.Initialise(hiddenLayer, null);
         }
 
+        /// <summary>
+        /// Set the input layer neuron at the given position
+        /// </summary>
+        /// <param name="position">
+        /// The position of the neuron value to set
+        /// </param>
+        /// <param name="value">
+        /// The new value of the neuron
+        /// </param>
         public void SetInput(int position, float value)
         {
             inputLayer.SetNeuronValue(position, value);
         }
 
+        /// <summary>
+        /// Set the output layer target at the given position
+        /// </summary>
+        /// <param name="position">
+        /// The position of the target value to set
+        /// </param>
+        /// <param name="value">
+        /// The new value of the target
+        /// </param>
         public void SetTarget(int position, float value)
         {
             outputLayer.SetTargetValue(position, value);
         }
 
+        /// <summary>
+        /// Feed the values forward through the network
+        /// </summary>
         public void FeedForward()
         {
             inputLayer.CalculateNeuronValues();
@@ -41,6 +96,12 @@ namespace ANN
             outputLayer.CalculateNeuronValues();
         }
 
+        /// <summary>
+        /// Return the aggregate error from the output layer
+        /// </summary>
+        /// <returns>
+        /// Aggregate error
+        /// </returns>
         public float CalculateError()
         {
             float error = 0F;
@@ -55,6 +116,9 @@ namespace ANN
             return error;
         }
 
+        /// <summary>
+        /// Back propagate the errors through the network
+        /// </summary>
         public void BackPropagate()
         {
             outputLayer.CalculateErrors();
@@ -64,6 +128,15 @@ namespace ANN
             inputLayer.AdjustWeights();
         }
 
+        /// <summary>
+        /// Return the output of the network from the output layer at the position
+        /// </summary>
+        /// <param name="position">
+        /// The position of the output to return
+        /// </param>
+        /// <returns>
+        /// The output of the network
+        /// </returns>
         public float GetOutput(int position)
         {
             return outputLayer.GetNeuronValue(position);
--- src/ANN/NeuronLayer.cs
+++ src/ANN/NeuronLayer.cs
@@ -6,27 +6,100 @@ using System.Threading.Tasks;
 
 namespace ANN
 {
+
+    /// <summary>
+    /// A single layer of the MLP
+    /// </summary>
     class NeuronLayer
     {
+        /// <summary>
+        /// The number of nodes in this layer
+        /// </summary>
         private int numberNodes;
+
+        /// <summary>
+        /// The number of nodes in the next layer
+        /// </summary>
         private int numberChildNodes;
+
+        /// <summary>
+        /// The number of nodes in the previous layer
+        /// </summary>
         private int numberParentNodes;
 
+        /// <summary>
+        /// The learning rate of the MLP
+        /// </summary>
         private float learningRate;
+
+        /// <summary>
+        /// The momentum factor of the MLP
+        /// </summary>
         private float momentumFactor;
 
+        /// <summary>
+        /// 1d array holding the individual neuron values
+        /// </summary>
         private float[] neuronValues;
+
+        /// <summary>
+        /// 1d array holding the individual target values
+        /// </summary>
         private float[] targetValues;
+
+        /// <summary>
+        /// 1d array holding the error for each value
+        /// </summary>
         private float[] errors;
 
+        /// <summary>
+        /// 2d array holding the weights
+        /// </summary>
         private float[,] weights;
+
+        /// <summary>
+        /// 2d array holding the changes in weight
+        /// </summary>
         private float[,] weightChanges;
+
+        /// <summary>
+        /// 1d array holding the individual bias values
+        /// </summary>
         private float[] biasValues;
+
+        /// <summary>
+        /// 1d array holding the weights for the bias
+        /// </summary>
         private float[] biasWeights;
 
+        /// <summary>
+        /// The previous layer object
+        /// </summary>
         private NeuronLayer parentLayer;
+
+        /// <summary>
+        /// The next layer object
+        /// </summary>
         private NeuronLayer childLayer;
 
+        /// <summary>
+        /// Constructor for the layer
+        /// </summary>
+        /// <param name="numberNodes">
+        /// The number of nodes in the layer
+        /// </param>
+        /// <param name="numberChildNodes">
+        /// The number of nodes in the next layer
+        /// </param>
+        /// <param name="numberParentNodes">
+        /// The number of nodes in the previous layer
+        /// </param>
+        /// <param name="learningRate">
+        /// The learning rate of the MLP
+        /// </param>
+        /// <param name="momentumFactor">
+        /// The momentum factor of the MLP
+        /// </param>
         public NeuronLayer(int numberNodes, int numberChildNodes, int numberParentNodes, float learningRate, float momentumFactor)
         {
             this.numberNodes = numberNodes;
@@ -61,44 +134,108 @@ namespace ANN
             }
         }
 
+        /// <summary>
+        /// Returns the number of nodes in the layer
+        /// </summary>
+        /// <returns>
+        /// The number of nodes in the layer
+        /// </returns>
         public int GetNumberNodes()
         {
             return numberNodes;
         }
 
+        /// <summary>
+        /// Return the neuron value at the given position
+        /// </summary>
+        /// <param name="position">
+        /// Position to return neuron value from
+        /// </param>
+        /// <returns>
+        /// The neuron value
+        /// </returns>
         public float GetNeuronValue(int position)
         {
             return neuronValues[position];
         }
 
+        /// <summary>
+        /// Set the value for a neuron at the given position
+        /// </summary>
+        /// <param name="position">
+        /// The position to set the neuron value
+        /// </param>
+        /// <param name="value">
+        /// The new value for the neuron
+        /// </param>
         public void SetNeuronValue(int position, float value)
         {
             neuronValues[position] = value;
         }
 
+        /// <summary>
+        /// Return the target value at the given position
+        /// </summary>
+        /// <param name="position">
+        /// The position to return the target value from
+        /// </param>
+        /// <returns>
+        /// The target value
+        /// </returns>
         public float GetTargetValue(int position)
         {
             return targetValues[position];
         }
 
+        /// <summary>
+        /// Set the target value at the given position
+        /// </summary>
+        /// <param name="position">
+        /// The position to set the target value
+        /// </param>
+        /// <param name="value">
+        /// The new value for the target
+        /// </param>
         public void SetTargetValue(int position, float value)
         {
             targetValues[position] = value;
         }
 
+        /// <summary>
+        /// Register the previous and next layers with the current layer
+        /// </summary>
+        /// <param name="parentLayer">
+        /// The previous layer
+        /// </param>
+        /// <param name="childLayer">
+        /// The next layer
+        /// </param>
         public void Initialise(NeuronLayer parentLayer, NeuronLayer childLayer)
         {
             this.parentLayer = parentLayer;
             this.childLayer = childLayer;
         }
 
+        /// <summary>
+        /// Return the sigmoid of the given action
+        /// </summary>
+        /// <param name="action">
+        /// The action value
+        /// </param>
+        /// <returns>
+        /// The value after sigmoid function applied to action
+        /// </returns>
         public float Sigmoid(float action)
         {
             return 1.0F / (1.0F + (float)Math.Exp(-action));
         }
 
+        /// <summary>
+        /// Calculate new values for all of the neurons in the layer
+        /// </summary>
         public void CalculateNeuronValues()
         {
+            // All layers besides input layer
             if (parentLayer != null)
             {
                 for (int j = 0; j < numberNodes; j++)
@@ -114,8 +251,12 @@ namespace ANN
             }
         }
 
+        /// <summary>
+        /// Calculate the errors for the layer
+        /// </summary>
         public void CalculateErrors()
         {
+            // Output layer
             if (childLayer == null)
             {
                 for (int i = 0; i < numberNodes; i++)
@@ -123,6 +264,7 @@ namespace ANN
                     errors[i] = (targetValues[i] - neuronValues[i]) * neuronValues[i] * (1.0F - neuronValues[i]);
                 }
             }
+            // Input layer
             else if (parentLayer == null)
             {
                 for (int i = 0; i < numberNodes; i++)
@@ -130,6 +272,7 @@ namespace ANN
                     errors[i] = 0.0F;
                 }
             }
+            // Hidden layers
             else
             {
                 for (int i = 0; i < numberNodes; i++)
@@ -144,8 +287,12 @@ namespace ANN
             }
         }
 
+        /// <summary>
+        /// Adjust the weights for the layer
+        /// </summary>
         public void AdjustWeights()
         {
+            // Not output layer
             if (childLayer != null)
             {
                 for (int i = 0; i < numberNodes; i++)
